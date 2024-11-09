e0e3545a511d253b59172bc7a20cf6011914c1cc
Shalom Craimer <scraimer@gmail.com>
2024-11-09 21:42:41 +0200

    Remove dead comment

--- deploy/tools/mine-efrat-page-for-zmanim/read-efrat-zmanim.py
+++ deploy/tools/mine-efrat-page-for-zmanim/read-efrat-zmanim.py
@@ -14,72 +14,8 @@ from pathlib import Path
 from typing import Any, Dict, List
 import pandas as pd
 
-# 3. Take the `efrat_zmanim.json` and put it with the rest of the back-end
 
 OUT_FILE = "efrat_zmanim.json"
-
-
-# def remove_spaces_and_invalid(row: Dict[str, Any]) -> Dict[str, Any]:
-#     # Remove extra whitespace in keys
-#     out1 = {k.strip(): v for k, v in row.items()}
-#     out = {}
-#     for k, v in out1.items():
-#         k = k.strip()
-#         if isinstance(v, str):
-#             v = v.strip()
-#         if v and pd.notna(v):
-#             out[k] = v
-#     return out
-
-
-# @dataclass
-# class FieldName:
-#     raw: str
-#     fixed: str
-
-
-# class FieldsToModify:
-#     DATE = FieldName(raw="תאריך לועזי", fixed="gregorian_date")
-#     NAME = FieldName(raw="הפרשה", fixed="name")
-
-
-# WANTED_UNCHANGED_FIELDS = {
-#     "הדלקת נרות": "candle_lighting",
-#     "צאת החג/השבת": "tzet_shabat",
-#     "תחילת הצום - 72 דקות": "fast_start",
-#     "סוף הצום": "fast_end",
-# }
-
-# skipped_fields = set()
-
-
-# def replace_timestamps(row: Dict[str, Any]) -> Dict[str, Any]:
-#     out = {}
-
-#     # Fix the datetime
-#     event_date: str = row[FieldsToModify.DATE.raw]
-#     event_date = event_date.replace(".9.", ".09.")
-#     d = datetime.datetime.strptime(event_date, "%d.%m.%y").date()
-#     out[FieldsToModify.DATE.fixed] = str(d)
-
-#     out[FieldsToModify.NAME.fixed] = row[FieldsToModify.NAME.raw]
-
-#     # prepare any dates or times to be serialized
-#     for k, v in row.items():
-#         if k not in WANTED_UNCHANGED_FIELDS:
-#             global skipped_fields
-#             if k not in skipped_fields:
-#                 skipped_fields.add(k)
-#             continue
-#         elif isinstance(v, datetime.time):
-#             v = v.strftime("%H:%M")
-#         out[WANTED_UNCHANGED_FIELDS[k]] = v
-
-#     return out
-
-
-# def normalize(row):
-#     return replace_timestamps(remove_spaces_and_invalid(row))
 
 
 def main():
